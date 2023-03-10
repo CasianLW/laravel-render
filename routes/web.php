bb71afe2046cd2ca11db1c00b8de79b469d5362b
CasianLW <ciorbcasian@gmail.com>
2023-03-10 12:36:54 +0100

    init laravel docker project for render

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// Route::get('/users', function () {
+//     $users = DB::table('users')->get();
+//     return view('users', compact('users'));
+// });
+// Route::get('/users', 'UserController@index');
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
